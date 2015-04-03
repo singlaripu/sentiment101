7224c10baf0687924a5f5e807dd33efbb71c230a
Ripu Singla <singlaripu@gmail.com>
2015-04-03 23:05:15 +0530

    adding heroku to allowed host

--- sentiment/settings.py
+++ sentiment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sentiment101.herokuapp.com']
 
 
 # Application definition
